73aef138b37cd33777456b6be438f57e41d2d0b9
JChien <jeffchien13@gmail.com>
2022-08-20 01:19:49 +0800

    graceful shutdown
    Signed-off-by: JChien <jeffchien13@gmail.com>

--- inference/inference.py
+++ inference/inference.py
@@ -3,7 +3,9 @@ from concurrent import futures
 import logging
 import os
 import pathlib
+import signal
 import sys
+import threading
 
 import grpc
 from joblib import load
@@ -41,6 +43,20 @@ class SklearnInferenceServicer(inference_pb2_grpc.InferenceServicer):
 
 
 #%%
+class GracefulExit:
+    def __init__(self, server: grpc.Server, timeout=10):
+        self.server = server
+        self.timeout = timeout
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, *args):
+        evt: threading.Event = self.server.stop(self.timeout)
+        evt.wait(self.timeout)
+        logger.info("Bye")
+
+
+#%%
 def serve():
     if not model_path or not pathlib.Path(model_path).exists():
         logger.error("model file not found.")
@@ -56,10 +72,8 @@ def serve():
     server.add_insecure_port(listen_on)
     server.start()
     logger.info(f"Listen on: {listen_on}")
-    try:
-        server.wait_for_termination()
-    except KeyboardInterrupt:
-        logger.info("Bye")
+    GracefulExit(server)
+    server.wait_for_termination()
     return 0
 
 
