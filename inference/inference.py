fd0ed89170da3409c71cadad9135b4a95b119e2a
JChien <jeffchien13@gmail.com>
2022-08-19 20:48:12 +0800

    docker file bug
    Signed-off-by: JChien <jeffchien13@gmail.com>

--- inference/inference.py
+++ inference/inference.py
@@ -1,16 +1,16 @@
 #%%
-import sys
+from concurrent import futures
 import logging
 import os
-from concurrent import futures
+import pathlib
+import sys
 
 import grpc
-import numpy as np
-import pandas as pd
-from gen.py.proto import inference_pb2_grpc, output_pb2
 from joblib import load
-from sklearn.feature_extraction import DictVectorizer
-import pathlib
+import numpy as np
+
+from gen.py.proto import inference_pb2_grpc
+from gen.py.proto import output_pb2
 
 logger = logging.getLogger("inference")
 logger.setLevel(logging.INFO)
@@ -22,9 +22,10 @@ logger.addHandler(ch)
 
 listen_on = os.environ.get("LISTEN_ON", "0.0.0.0:9527")
 model_path = os.environ.get("MODEL_PATH")
+model_framework = os.environ.get("MODEL_FRAMEWORK")
 
 #%%
-class InferenceServicer(inference_pb2_grpc.InferenceServicer):
+class SklearnInferenceServicer(inference_pb2_grpc.InferenceServicer):
     def __init__(self):
         self.model = load(model_path)
 
@@ -42,21 +43,26 @@ class InferenceServicer(inference_pb2_grpc.InferenceServicer):
 #%%
 def serve():
     if not model_path or not pathlib.Path(model_path).exists():
-        logger.error(f"model file not found.")
-        return
+        logger.error("model file not found.")
+        return 1
+
+    servicer = {"sklearn": SklearnInferenceServicer}.get(model_framework)
+    if not servicer:
+        logger.error(f"unknown model framework: {model_framework}")
+        return 1
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    inference_pb2_grpc.add_InferenceServicer_to_server(InferenceServicer(), server)
+    inference_pb2_grpc.add_InferenceServicer_to_server(servicer(), server)
     server.add_insecure_port(listen_on)
     server.start()
     logger.info(f"Listen on: {listen_on}")
     try:
         server.wait_for_termination()
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         logger.info("Bye")
-        sys.exit(0)
+    return 0
 
 
 #%%
 if __name__ == "__main__":
-    serve()
+    sys.exit(serve())
