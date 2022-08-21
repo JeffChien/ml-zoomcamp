772ec70b8783c851271d646b20b582695400d16d
JChien <jeffchien13@gmail.com>
2022-08-21 17:06:17 +0800

    tidy api
    Signed-off-by: JChien <jeffchien13@gmail.com>

--- gateway.py
+++ gateway.py
@@ -7,7 +7,7 @@ from ch2 import api as ch2_api
 app = Flask(__name__)
 
 
-@app.route("/ch2_car_price:predict", methods=["POST"])
+@app.route("/ch2/car-price/predict", methods=["POST"])
 def ch2_car_price_predict():
     X = ch2_api.preprocess(request.data)
     resp = ch2_api.predict(X)
